dbcc4e687441d64d79c75e2ae829c76023ab3127
Szabó Liza <szbliza97@gmail.com>
2024-03-15 21:18:59 +0100

    Services & Cart Button located

--- test.js
+++ test.js
@@ -11,6 +11,15 @@ async function shoppingCartTest() {
         let shopLink = await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/header/div[1]/div/nav/a[1]/span')))
         await shopLink.click()
 
+        let services = await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/main/div[2]/div[1]/button')))
+        await services.click()
+
+        let cartButton = await driver.wait(until.elementLocated(By.xpath('//*[@id="calendlyRoot"]/div[1]/div[1]/div[3]/button[2]')))
+        await cartButton.click()
+        
+
+
+
     }catch(error){
 
         console.log(error)
