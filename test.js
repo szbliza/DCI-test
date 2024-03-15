ae7f4e1adcb12653d1f0beacc6ef9bc8866aa9fd
Szabó Liza <szbliza97@gmail.com>
2024-03-15 21:05:14 +0100

    Navigating to Shop finally works

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {Builder, By, Browser} = require('selenium-webdriver')
+const {Builder, By, Browser, until} = require('selenium-webdriver')
 
 async function shoppingCartTest() {
 
@@ -8,10 +8,12 @@ async function shoppingCartTest() {
 
         await driver.get("https://tst.datatronic.cloud")
         
-        let shopLink = await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/header/div[1]/div/nav/a[1]')))
+        let shopLink = await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/header/div[1]/div/nav/a[1]/span')))
         await shopLink.click()
 
     }catch(error){
+
+        console.log(error)
 
     }
 
